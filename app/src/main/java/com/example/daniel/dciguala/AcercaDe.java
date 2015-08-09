f4398e826dcfe765ab119a2f3b139cc5bfd5b047
Mashi <danisjv@gmail.com>
2015-08-09 13:09:35 -0500

    Project setup

---
+++ app/src/main/java/com/example/daniel/dciguala/AcercaDe.java
@@ -0,0 +1,19 @@
+package com.example.daniel.dciguala;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daniel on 03/08/2015.
+ */
+public class AcercaDe extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.acercade, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/daniel/dciguala/Mapa.java
@@ -0,0 +1,27 @@
+package com.example.daniel.dciguala;
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by Daniel on 03/08/2015.
+ */
+public class Mapa extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.mapa, container, false);
+        return rootView;
+    }
+}
